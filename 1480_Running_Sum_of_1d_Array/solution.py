d1d41bb68eecb07c52bec076050aa9161c6bb9cb
Mayank Kumar <mayankkumar270607@gmail.com>
2025-11-28 10:42:26 +0530

    Add solution for 1480

---
+++ 1480_Running_Sum_of_1d_Array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        result[0] = nums[0]
+
+        for i in range(1, n):
+            result[i] = result[i - 1] + nums[i]
+
+        return result
