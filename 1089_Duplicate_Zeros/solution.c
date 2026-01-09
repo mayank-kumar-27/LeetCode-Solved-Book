f3e08c4d0b9fb0a714b885a9a196c9957e00cac5
Mayank Kumar <mayankkumar270607@gmail.com>
2026-01-09 10:00:02 +0530

    Add solution for 1089

---
+++ 1089_Duplicate_Zeros/solution.c
@@ -0,0 +1,9 @@
+void duplicateZeros(int* arr, int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        if (arr[i] == 0 && i + 1 < arrSize) {
+            for (int j = arrSize - 1; j > i; j--) arr[j] = arr[j - 1];
+            arr[i + 1] = 0;
+            i++;
+        }
+    }
+}
---
+++ 1089_Duplicate_Zeros/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        n = len(arr)
+        i = 0
+        while i < n:
+            if arr[i] == 0 and i + 1 < n:
+                for j in range(n - 1, i, -1):
+                    arr[j] = arr[j - 1]
+                arr[i + 1] = 0
+                i += 2
+            else:
+                i += 1
