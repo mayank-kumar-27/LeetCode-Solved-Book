2ac108026d9e81124006702d9b6c2e8f6e4daccb
Mayank Kumar <mayankkumar270607@gmail.com>
2026-09-09 21:18:18 +0530

    Time: 2 ms (97.67%), Space: 72.8 MB (5.96%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean lemonadeChange(int[] b) {
+        int f = 0, t = 0;
+        for (int x : b) {
+            if (x == 5) f++;
+            else if (x == 10) {
+                if (f == 0) return false;
+                f--; t++;
+            } else {
+                if (t > 0 && f > 0) { t--; f--; } 
+                else if (f >= 3) f -= 3;
+                else return false;
+            }
+        } return true;
+    }
+}
