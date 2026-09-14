803c730c16a2ee99c7bae3d874532199d4b88b48
Mayank Kumar <mayankkumar270607@gmail.com>
2026-09-14 21:27:19 +0530

    Time: 0 ms (100%), Space: 42 MB (54.7%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1, b = 1;
+        for (int i = 2; i <= n; i++) {
+            b += a; a = b - a;
+        } return b;
+    }
+}
